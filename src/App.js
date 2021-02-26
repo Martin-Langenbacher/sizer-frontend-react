75c900db156bff30071defb8ae443e82fa1c83e0
Markus Schneider <markus.schneider@wildcodeschool.com>
2021-02-26 14:11:32 +0100

    Added backend call to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/users")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setUsers(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. --> Markus
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Users
+        <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.email}
+          </li>
+        ))}
+      </ul>
       </header>
     </div>
   );
